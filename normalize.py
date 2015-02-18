22880b3a823613f9fd168ead2f59c15f7d6c0050
jchazalo <jchazalo@5ca474d0-9d90-11e4-8793-ebb5b8661baa>
2015-02-18 12:04:29 +0000

    first version with appropriate normalization
    git-svn-id: https://svn.univ-lr.fr/ulr/icdar15-moc/moc_norm/trunk@68 5ca474d0-9d90-11e4-8793-ebb5b8661baa

--- normalize.py
+++ normalize.py
@@ -15,13 +15,9 @@ Sample usage:
 # Imports
 import logging
 import argparse
-import os
-import os.path
 import sys
-import fileinput
-from collections import defaultdict
-import re
-import csv
+import unicodedata
+import io
 
 # ==============================================================================
 # Logging
@@ -30,8 +26,8 @@ logger = logging.getLogger(__name__)
 # ==============================================================================
 # Constants
 PROG_VERSION = "0.1"
-PROG_NAME = "OCR Result Normalized for ICDAR15 SmartDOC"
-PROG_NAME_SHORT = "moc_norm"
+PROG_DESCR = "OCR Result Normalizer for ICDAR15 SmartDOC"
+PROG_NAME = "moc_norm"
 
 ERRCODE_OK = 0
 ERRCODE_NOFILE = 10
@@ -53,7 +49,7 @@ def _programHeader(logger, prog_name, prog_version):
     logger.debug(dbg_head_pre + dbg_head)
 
 def _initLogger(logger, debug=False):
-    format="%(name)-12s %(levelname)-7s: %(message)s" #%(module)-10s
+    format="%(module)-9s %(levelname)-7s: %(message)s"
     formatter = logging.Formatter(format)    
     ch = logging.StreamHandler()  
     ch.setFormatter(formatter)  
@@ -69,15 +65,15 @@ def main():
     # Option parsing
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description=PROG_NAME, 
+        description=PROG_DESCR, 
         version=PROG_VERSION)
     parser.add_argument('-d', '--debug', 
         action="store_true", 
         help="Activate debug output.")
-    parser.add_argument('-o', '--output', 
-        help="Optional path to output file.")
     parser.add_argument('input', 
         help='Input text file with UTF-8 encoding.')
+    parser.add_argument('-o', '--output', 
+        help="Optional path to output file.")
     args = parser.parse_args()
 
     # -----------------------------------------------------------------------------
@@ -94,10 +90,25 @@ def main():
     logger.debug(_DBGSEP)
 
     # --------------------------------------------------------------------------
-    logger.debug("--- Process started. ---")
-    
-    
-    
+    file_output = None
+    fo_is_real_file = False
+    try:
+        if args.output:
+            file_output = io.open(args.output, 'wt')
+            fo_is_real_file = True
+        else:
+            file_output = sys.stdout
+
+        logger.debug("--- Process started. ---")
+        with io.open(args.input, 'rt') as file_input:
+            for line in file_input:
+                # TODO check input
+                # TODO perform extra normalization (tab to space, quotes, pipe)
+                print >>file_output, unicodedata.normalize('NFKC', line),
+    finally:
+        if fo_is_real_file:
+            file_output.close()
+
 
     logger.debug("--- Process complete. ---")
     # --------------------------------------------------------------------------
